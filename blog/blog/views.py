d86a80cd14473ed63f7d29f8fec1ea09ee02203c
anushajain2 <fhl.anusha@gmail.com>
2019-08-24 16:48:39 +0530

    Added urls and views

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    #render the request using the template provided
+    return render(request, 'home.html')
+
 
 def about(request):
-''' request is the object received from the browser '''
-    return HttpResponse("about")
+    #request is the object received from the browser
+    return render(request, 'about.html')
